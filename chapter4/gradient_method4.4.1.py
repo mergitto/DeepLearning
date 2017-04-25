d5f8a5e1063dc2f8919653ef0e07a3230ae1bf6b
meru <s13a1001@bene.fit.ac.jp>
2017-04-25 10:23:19 +0900

    4章のまとめ

--- chapter4/gradient_method4.4.1.py
+++ chapter4/gradient_method4.4.1.py
@@ -1,3 +1,4 @@
+# 勾配降下法の実装
 # coding: utf-8
 import numpy as np
 import matplotlib.pylab as plt
@@ -20,7 +21,7 @@ def gradient_descent(f, init_x, lr=0.01, step_num=100):
 def function_2(x):
     return x[0]**2 + x[1]**2
 
-init_x = np.array([-3.0, 4.0])    
+init_x = np.array([-3.0, 4.0])
 
 lr = 0.1
 step_num = 20
