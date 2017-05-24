58cf1a4a682cf753973c0be7372fe38a7f61fd92
mergitto <mfm17101@bene.fit.ac.jp>
2017-05-24 20:08:29 +0900

    変更分をプッシュ！

--- pyTrain/csvHddRead.py
+++ pyTrain/csvHddRead.py
@@ -6,8 +6,6 @@ import csv
 import pandas as pd
 
 dataset = pd.read_csv("./pyDays.csv")
-print(dataset)
-
 
 f = open('./pyDays.csv', 'r')
 dataReader = csv.reader(f)
@@ -15,7 +13,7 @@ days = np.array( [] )
 
 for gyou,row in enumerate(dataReader):
     for index, data in enumerate(row):
-        if(index == 20 and gyou != 0): # プロジェクト作成日の行でタイトル行じゃないとき
+        if(index == 0 and gyou != 0): # プロジェクト作成日の行でタイトル行じゃないとき
             day = datetime.datetime.strptime(data, '%m/%d/%y')
             if(day < datetime.datetime.strptime('2016-10-01', '%Y-%m-%d')): #2016-10-01までのデータを使用する
                 days = np.append(days, datetime.datetime.strptime(data, '%m/%d/%y'))
@@ -28,10 +26,11 @@ for word, cnt in counter.most_common():
     words = np.append(words, word)
     counts = np.append(counts, cnt)
 
+datum = np.zeros((0,words.size))
 datum = np.r_[datum, words.reshape(1, -1)]
 datum = np.r_[datum, counts.reshape(1, -1)]
 
-#print(datum[0])
+#print(datum)
 
 #plt.plot(datum[0], datum[1])
 #plt.show()
